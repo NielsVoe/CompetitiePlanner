7db367ce7627028615e8cb999dc19a0b1225d94b
Niels Voermans <voermans.niels@gmail.com>
2025-06-24 00:39:29 +0200

    Summarize in table

--- pages/mainPage.py
+++ pages/mainPage.py
@@ -2,6 +2,7 @@ import streamlit as st
 import CompetitiePlanner as CP
 from src.Club import Club
 from src.Gender import Gender
+import pandas as pd
 
 st.title("Uitleg Competitie Planner")
 
@@ -10,20 +11,26 @@ Welkom bij de Competitie Planner voor BC Geldrop!
 """)
 
 if st.button("Ververs data"):
-    st.write("Scraping data from:", CP.GetUrl())
     try:
         CP.RetrieveData()
     except Exception as e:
         st.error(f"An unexpected error occurred: {e}")
     
-    club:Club = CP.GetClub()
-    for team in club.GetTeams():
-        st.write(f"Team: {team.name}")
-        st.write(f"  Heren:")
-        for player in team.players:
-            if player.gender == Gender.MALE:
-                st.write(player.name)
-        st.write(f"  Dames:")
-        for player in team.players:
-            if player.gender == Gender.FEMALE:
-                st.write(player.name)
+    df = pd.DataFrame({
+        "Team": [team.name for team in CP.GetClub().GetTeams()],
+        "Aantal spelers": [len(team.players) for team in CP.GetClub().GetTeams()]
+    })
+
+    st.table(df)
+    
+    # club:Club = CP.GetClub()
+    # for team in club.GetTeams():
+    #     st.write(f"Team: {team.name}")
+    #     st.write(f"  Heren:")
+    #     for player in team.players:
+    #         if player.gender == Gender.MALE:
+    #             st.write(player.name)
+    #     st.write(f"  Dames:")
+    #     for player in team.players:
+    #         if player.gender == Gender.FEMALE:
+    #             st.write(player.name)
