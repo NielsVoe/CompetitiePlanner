377e62b79b6024a57a3313b1257c7ad5efeb7404
Niels Voermans <voermans.niels@gmail.com>
2025-06-23 16:07:08 +0200

    Testing

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ url = st.text_input("Enter a URL to scrape", "https://example.com")
 
 if st.button("Scrape"):
     st.write("Scraping data from:", url)
-    # try:
-    #     CP.RetrieveData()
-    #     st.success("Data scraped successfully!")
-    # except Exception as e:
-    #     st.error(f"An unexpected error occurred: {e}")
+    try:
+        CP.RetrieveData()
+        st.success("Data scraped successfully!")
+    except Exception as e:
+        st.error(f"An unexpected error occurred: {e}")
     
-    # club:Club = CP.GetClub()
-    # for team in club.teams:
-    #     st.write(f"Team: {team.name}")
-    #     st.write(f"  Heren:")
-    #     for player in team.players:
-    #         if player.gender == Gender.MALE:
-    #             st.write(player.name)
-    #     st.write(f"  Dames:")
-    #     for player in team.players:
-    #         if player.gender == Gender.FEMALE:
-    #             st.write(player.name)
+    club:Club = CP.GetClub()
+    for team in club.teams:
+        st.write(f"Team: {team.name}")
+        st.write(f"  Heren:")
+        for player in team.players:
+            if player.gender == Gender.MALE:
+                st.write(player.name)
+        st.write(f"  Dames:")
+        for player in team.players:
+            if player.gender == Gender.FEMALE:
+                st.write(player.name)
 
     
