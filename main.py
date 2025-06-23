38c837fd6207466b6544652cc4659fc777d0dc56
Niels Voermans <voermans.niels@gmail.com>
2025-06-24 00:22:19 +0200

    Changed location of refresh data

--- main.py
+++ main.py
@@ -12,26 +12,3 @@ teamPage = st.Page("pages/teamPage.py", title="Teams")
 pg = st.navigation([mainPage, teamPage])
 
 pg.run()
-
-# URL input
-
-if st.button("Scrape"):
-    st.write("Scraping data from:", CP.GetUrl())
-    try:
-        CP.RetrieveData()
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
-    
-    club:Club = CP.GetClub()
-    for team in club.GetTeams():
-        st.write(f"Team: {team.name}")
-        st.write(f"  Heren:")
-        for player in team.players:
-            if player.gender == Gender.MALE:
-                st.write(player.name)
-        st.write(f"  Dames:")
-        for player in team.players:
-            if player.gender == Gender.FEMALE:
-                st.write(player.name)
-
-    
--- pages/mainPage.py
+++ pages/mainPage.py
@@ -1,7 +1,29 @@
 import streamlit as st
+import CompetitiePlanner as CP
+from src.Club import Club
+from src.Gender import Gender
 
 st.title("Uitleg Competitie Planner")
 
 st.write("""
 Welkom bij de Competitie Planner voor BC Geldrop!
 """)
+
+if st.button("Ververs data"):
+    st.write("Scraping data from:", CP.GetUrl())
+    try:
+        CP.RetrieveData()
+    except Exception as e:
+        st.error(f"An unexpected error occurred: {e}")
+    
+    club:Club = CP.GetClub()
+    for team in club.GetTeams():
+        st.write(f"Team: {team.name}")
+        st.write(f"  Heren:")
+        for player in team.players:
+            if player.gender == Gender.MALE:
+                st.write(player.name)
+        st.write(f"  Dames:")
+        for player in team.players:
+            if player.gender == Gender.FEMALE:
+                st.write(player.name)
