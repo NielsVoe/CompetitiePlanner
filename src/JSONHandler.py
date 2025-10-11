cb537b57c4ff0763692fed11b13845dce17f119f
Niels Voermans <n.a.voermans@student.tue.nl>
2025-10-12 00:19:11 +0200

    Refactor Import method to include directory parameter and update docstring

--- src/JSONHandler.py
+++ src/JSONHandler.py
@@ -8,6 +8,7 @@ class JSONHandler:
         Export data to a JSON file.
         
         :param data: List of dictionaries containing the data to export.
+        :param directory: The directory where the file will be saved.
         :param filename: The name of the file to save the JSON data to.
         """
         if not os.path.isdir(directory):
@@ -17,16 +18,17 @@ class JSONHandler:
         print(f"Data exported to {os.path.join(directory, filename)} successfully.")
 
     @staticmethod
-    def Import(filename: str) -> list[dict]:
+    def Import(directory: str, filename: str) -> list[dict]:
         """
         Import data from a JSON file.
         
+        :param directory: The directory where the file is located.
         :param filename: The name of the file to read the JSON data from.
         :return: List of dictionaries containing the imported data.
         """
-        if not os.path.isfile(filename):
+        if not os.path.isfile(os.path.join(directory, filename)):
             raise FileNotFoundError(f"The file {filename} does not exist.")
-        with open(filename, 'r', encoding='utf-8') as f:
+        with open(os.path.join(directory, filename), 'r', encoding='utf-8') as f:
             data = json.load(f)
         print(f"Data imported from {filename} successfully.")
         return data
